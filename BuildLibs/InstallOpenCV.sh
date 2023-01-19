00cad65f339079040063d8a10a0abc354ca29ad5
Chanwoo Lee <leechanwoo25@gmail.com>
2023-01-19 21:06:07 +0900

    Add cpu event odometry image but failed

--- BuildLibs/InstallOpenCV.sh
+++ BuildLibs/InstallOpenCV.sh
@@ -5,15 +5,15 @@ export DEBIAN_FRONTEND=noninteractive
 cd ~
 
 sudo apt-get update &&
-sudo apt-get-get upgrade -y &&
+sudo apt-get dist-upgrade -y &&
 sudo apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
       cmake build-essential git \
       gfortran libprotobuf-dev protobuf-compiler \
-      libblas-dev liblapack-dev liblapacke-dev \
+      libblas-dev libopenblas-dev liblapack-dev liblapacke-dev \
       libjpeg-dev libpng-dev libtiff-dev libtbb-dev libgtk2.0-dev pkg-config \
-      ffmpeg libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev
-      libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev &&
-sudo apt-get-get clean &&
+      ffmpeg libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev \
+      libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev libgl1-mesa-dev &&
+sudo apt-get clean &&
 sudo apt-get autoremove -y &&
 
 # Install Ceres
@@ -30,13 +30,14 @@ rm -rf ceres-solver
 
 # Install OpenCV
 # Ceres 2.1.0 has some new function that build issue occurs when building sfm
-export OPENCV_VERSION=3.4.18
-RUN cd ~ &&
-git config --global http.sslverify false &&
-git clone https://github.com/opencv/opencv.git --branch ${OPENCV_VERSION} --single-branch &&
-git clone https://github.com/opencv/opencv_contrib.git --branch ${OPENCV_VERSION} --single-branch &&
-cd opencv &&
-mkdir build && cd build \
+export OPENCV_VERSION=3.3.0
+cd ~
+# git config --global http.sslverify false &&
+git clone https://github.com/opencv/opencv.git --branch ${OPENCV_VERSION} --single-branch
+git clone https://github.com/opencv/opencv_contrib.git --branch ${OPENCV_VERSION} --single-branch
+
+cd opencv
+mkdir build && cd build
 
 # atmost packages will be detected automatically
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
